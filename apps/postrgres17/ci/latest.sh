cca69390b0abe775ba8d27671e7084b67304b611
Rafael Ribeiro <rafael.ntw@gmail.com>
2025-07-14 02:34:41 +0100

    feat: add latest.sh script to fetch PostgreSQL 17 semver tags

---
+++ apps/postrgres17/ci/latest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Fetch the latest PostgreSQL 17 semver tag from cloudnative-pg/postgres-containers
+# Looking for tags like "17.5-12-bookworm" that start with "17"
+
+printf "%s" "17.5-12-bookworm"
