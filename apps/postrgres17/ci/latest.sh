b1b9dce4a54416bf868782475f4f24e75803e14d
Rafael Ribeiro <rafael.ntw@gmail.com>
2026-05-26 10:18:32 +0100

    chore: remove apps not referenced in home-ops

--- apps/postrgres17/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Fetch the latest PostgreSQL 17 semver tag from cloudnative-pg/postgres-containers
-# Looking for tags like "17.5-12-bookworm" that start with "17"
-
-printf "%s" "17.5-12-bookworm"
